4fec905b99b7ab7295cbf885177a1fb00a24c62a
lyx <woshiluyangxing@qq.com>
2019-02-24 11:19:23 +0800

    第2次提交 使用了mybatis 修复了中文乱码

--- src/main/java/com/hhuc/wechat/x/Service/Qubi.java
+++ src/main/java/com/hhuc/wechat/x/Service/Qubi.java
@@ -6,6 +6,7 @@ package com.hhuc.wechat.x.Service;
  * @create: 2019-02-22 17:10
  **/
 import com.hhuc.wechat.x.DAO.Userexer;
+import org.apache.log4j.Logger;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -24,6 +25,7 @@ import javax.servlet.http.HttpServletResponse;
 public class Qubi  extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
+    private static Logger logger = Logger.getLogger(Qubi.class);
 
     /**
      * @see HttpServlet#HttpServlet()
@@ -33,6 +35,13 @@ public class Qubi  extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
+    public void init() {
+        logger.debug("This is debug message.");
+        // 记录info级别的信息
+        logger.info("This is info message.");
+        // 记录error级别的信息
+        logger.error("This is error message.");
+    }
     /**
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
@@ -50,9 +59,11 @@ public class Qubi  extends HttpServlet {
 //
 //        String res = q.query_openid();
         Userexer userexer = new Userexer(data);
-        System.out.println(data[0]);
         String res = userexer.queryOpenid();
+        logger.error("This is dsds message.");
+        out.println("查询00结果");
         out.println(res);
+
     }
 
     // 处理 POST 方法请求的方法
