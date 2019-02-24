5b72249843a359f32fd4db435f3140227e0d8d39
lyx <woshiluyangxing@qq.com>
2019-02-24 11:45:01 +0800

    增加了log4j

--- src/main/java/com/hhuc/wechat/x/Service/Qubi.java
+++ src/main/java/com/hhuc/wechat/x/Service/Qubi.java
@@ -6,8 +6,8 @@ package com.hhuc.wechat.x.Service;
  * @create: 2019-02-22 17:10
  **/
 import com.hhuc.wechat.x.DAO.Userexer;
+import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
@@ -27,6 +27,7 @@ public class Qubi  extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private static Logger logger = Logger.getLogger(Qubi.class);
 
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -35,13 +36,7 @@ public class Qubi  extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-    public void init() {
-        logger.debug("This is debug message.");
-        // 记录info级别的信息
-        logger.info("This is info message.");
-        // 记录error级别的信息
-        logger.error("This is error message.");
-    }
+
     /**
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
@@ -61,7 +56,12 @@ public class Qubi  extends HttpServlet {
         Userexer userexer = new Userexer(data);
         String res = userexer.queryOpenid();
         logger.error("This is dsds message.");
-        out.println("查询00结果");
+        logger.debug("This is debug message.");
+        // 记录info级别的信息
+        logger.info("This is info message.");
+        // 记录error级别的信息
+        logger.error("This is error message.");
+        out.println("查询结果"+"\n");
         out.println(res);
 
     }
