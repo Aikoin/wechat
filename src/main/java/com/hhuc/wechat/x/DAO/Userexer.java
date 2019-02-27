b304a445bd5e40086f0307c869b3c8367c40d51d
lyx <woshiluyangxing@qq.com>
2019-02-27 22:41:45 +0800

    增加了修改资料的api 修改了之前的数据结构
    Signed-off-by: lyx <woshiluyangxing@qq.com>

--- src/main/java/com/hhuc/wechat/x/DAO/Userexer.java
+++ src/main/java/com/hhuc/wechat/x/DAO/Userexer.java
@@ -18,27 +18,13 @@ import java.io.InputStream;
 public class Userexer<sout> {
 
     private String parameters[];
-    public Userexer(String Parameters[]) {
-        parameters = Parameters;
+    public Userexer(User user) {
+        this.user =user;
     }
-    public String getOpenid() {
-        return parameters[0];//
-    }
+
+
+    private int res;
     private User user;
-    public String queryOpenid() throws IOException {
-        SqlSession session = getSqlSession();
-        String openid =getOpenid();
-
-        try {
-            UserMapper Mapper = session.getMapper(UserMapper.class);
-            user = Mapper.queryOpenid(openid).get(0);
-            System.out.println(user);
-        } finally {
-            session.close();
-        }
-        return user.toString();
-    }
-
     private SqlSession getSqlSession() throws IOException {
         String resource = "com/hhuc/wechat/x/conf/mybatis_config.xml";
         InputStream inputStream = Resources.getResourceAsStream(resource);
@@ -46,4 +32,68 @@ public class Userexer<sout> {
                 new SqlSessionFactoryBuilder().build(inputStream);
         return sqlSessionFactory.openSession();
     }
+
+//    public String modifyPersonData() throws IOException {
+//        SqlSession session = getSqlSession();
+//        String openid =getOpenid();
+//        String phone =getPhone();
+//        String province =getProvince();
+//        String city =getCity();
+//        String language =getLanguage();
+//        String avatarUrl =getAvatarUrl();
+//
+//        try {
+//            UserMapper Mapper = session.getMapper(UserMapper.class);
+//            res = Mapper.updateUserIfNecessary();
+//            System.out.println(res);
+//        } finally {
+//            session.close();
+//        }
+//        return "修改成功"+res;
+//    }
+
+    public String queryOpenid() throws IOException {
+        SqlSession session = getSqlSession();
+
+        try {
+            UserMapper Mapper = session.getMapper(UserMapper.class);
+            user = Mapper.queryOpenid(user.getOpenid());
+            System.out.println(user);
+        } finally {
+            session.close();
+        }
+        return user.toString();
+    }
+
+    public String updateUser() throws IOException {
+        SqlSession session = getSqlSession();
+        
+
+        try {
+            UserMapper Mapper = session.getMapper(UserMapper.class);
+            System.out.println(            user.toString()
+            );
+            Mapper.updateUser(user);
+            session.commit();
+            System.out.println("修改完成");
+        } finally {
+
+            session.close();
+        }
+        return user.toString();
+    }
+
+    public String addUser() throws IOException {
+        SqlSession session = getSqlSession();
+        try {
+            UserMapper Mapper = session.getMapper(UserMapper.class);
+            Mapper.addUser(user);
+            session.commit();
+            System.out.println("增加完成");
+        } finally {
+            session.close();
+        }
+        return user.toString();
+    }
+
 }
